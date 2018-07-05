bf459f6be9a8aef076d201db01cbb7e279cd3b0d
GinaMaddox <gina.maddox@gmail.com>
2018-07-04 23:26:13 -0400

    delete and update buttons

--- app/controllers/client/books_controller.rb
+++ app/controllers/client/books_controller.rb
@@ -34,4 +34,24 @@ class Client::BooksController < ApplicationController
     @book = response.body
     render "edit.html.erb"
   end
+
+  def update
+    book_id = params[:id]
+    response = Unirest.patch("localhost:3000/api/books/#{book_id}", parameters: {
+        title: params[:title] ,
+        author: params[:author],
+        publisher: params[:publisher] ,
+        genre: params[:genre]
+    })
+    @book = response.body
+    render "show.html.erb"
+  end
+
+  def destroy
+    book_id = params[:id]
+    response = Unirest.delete("localhost:3000/api/books/#{book_id}")
+    render "destroy.html.erb"
+  end
+
+
 end
